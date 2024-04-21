fa4b4d54939d659fe9415dd2cb6e03f500e6b17b
LAPTOP-3R4ARCBV\MrJin <aryanjindal103@gmail.com>
2024-04-21 20:45:49 +0530

    Solved

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,9 +1,23 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.function.Supplier;
+
 public class VendingMachine {
 
+    private static final HashMap<String, Supplier<Snack>> hm=new HashMap<>();
+
+    static{
+        hm.put("Drink",Drink::new);
+        hm.put("Chips",Chips::new);
+        hm.put("ChocolateBar",ChocolateBar::new);
+    }
+
     Snack getSnack(String snackType) {
-        return null;
+        if(hm.get(snackType)!=null)
+            return hm.get(snackType).get();
+        else
+            throw new IllegalArgumentException("Invalid Snack");
     }
 
 
